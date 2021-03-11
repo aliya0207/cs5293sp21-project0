86c534d7cea6447eac2cb88d37584fae33f893e3
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:29:20 -0600

    Create test_extract.py

---
+++ tests/test_extract.py
@@ -0,0 +1,9 @@
+import pytest
+import argparse
+from project0 import project0
+url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
+
+def test_extract_incidents():
+    project0.fetchincidents(url)
+    l = project0.extractincidents()
+    assert len(l) > 0
