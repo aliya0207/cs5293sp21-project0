403502120368c7201a53fd4fa48a1f208f6e1166
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:42:38 -0600

    Update test_createdb.py

--- tests/test_createdb.py
+++ tests/test_createdb.py
@@ -1,5 +1,6 @@
 import pytest
 import argparse
+import project0
 from project0 import project0
 url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
 def test_createdb():
