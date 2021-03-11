359ba039adf019d6d2cee8d554d155b86c180394
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 23:30:46 -0600

    Delete test1.py

--- tests/test1.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import argparse
-import project0
-from project0 import project0
-
-def test_fetchincidents():
-    url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
-    assert project0.fetchincidents(url) is not None
