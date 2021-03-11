d572dfb5be7e4a24b2138c5622659787881afc1b
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:49:36 -0600

    Update test_populatedb.py

--- tests/test_populatedb.py
+++ tests/test_populatedb.py
@@ -1,6 +1,7 @@
 import pytest
 import argparse
 import project0
+import sqlite3
 from project0 import project0
 url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
 
