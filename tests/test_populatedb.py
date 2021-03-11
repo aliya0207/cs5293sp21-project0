bfd5ca259fac76cc6ce5c87fa9e6473712792f79
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:26:42 -0600

    Create test_populatedb.py

---
+++ tests/test_populatedb.py
@@ -0,0 +1,11 @@
+import pytest
+import argparse
+from project0 import project0
+url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
+
+def test_populatedb():
+    sql = sqlite3.connect(db)
+    a = sql.cursor()
+    a.execute('SELECT * FROM incidents')
+    result = cur.fetchall()
+    assert a is not None   
