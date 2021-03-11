cd6412fc943bf1f7a8e6fdf14b0ecea8056f380d
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 23:00:48 -0600

    Update test_populatedb.py

--- tests/test_populatedb.py
+++ tests/test_populatedb.py
@@ -4,10 +4,10 @@ import project0
 import sqlite3
 from project0 import project0
 url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
-
+db='normanpd.db'
 def test_populatedb():
     sql = sqlite3.connect(db)
     a = sql.cursor()
     a.execute('SELECT * FROM incidents')
-    result = cur.fetchall()
-    assert a is not None   
+    result = a.fetchall()
+    assert result is not None   
