378e54e40baaf11c229938bbc33b9e5857741f26
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:53:54 -0600

    Update test_extract.py

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,10 +1,13 @@
 import pytest
 import argparse
 import project0
+import sqlite3
+
 from project0 import project0
 url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
 
 def test_extract_incidents():
-    project0.fetchincidents(url)
-    l = project0.extractincidents()
+    
+    a=project0.fetchincidents(url)
+    l = project0.extractincidents(a)
     assert len(l) > 0
