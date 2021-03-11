40010e1ec47e935fe957291f2d8d44d8259c1d4e
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:55:39 -0600

    Update test_statusdb.py

--- tests/test_statusdb.py
+++ tests/test_statusdb.py
@@ -1,6 +1,8 @@
 import pytest
 import argparse
 import project0
+import sqlite3
+
 from project0 import project0
 url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
 
@@ -8,7 +10,7 @@ def test_status():
     incident_data= project0.fetchincidents(url)
     incidents= project0.extractincidents(incident_data)
     db= project0.createdb()
-    output= project0.statusdb()
+    output= project0.status(db)
     msg= "Error. test not satisfied."
     assert output is not None
     assert isinstance(output,list,msg)
