ae852cc82dfba651eef8f542bc962db64989e63c
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-03-10 22:28:23 -0600

    Create test_statusdb.py

---
+++ tests/test_statusdb.py
@@ -0,0 +1,14 @@
+import pytest
+import argparse
+from project0 import project0
+url = "https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf"
+
+def test_status():
+    incident_data= project0.fetchincidents(url)
+    incidents= project0.extractincidents(incident_data)
+    db= project0.createdb()
+    output= project0.statusdb()
+    msg= "Error. test not satisfied."
+    assert output is not None
+    assert isinstance(output,list,msg)
+    
